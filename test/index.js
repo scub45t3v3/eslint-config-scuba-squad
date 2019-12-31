3885b199e2ffd2b8d5e34afb93c9b6123ee529d8
Steve Winberg <stevewin21@gmail.com>
2019-12-31 13:17:31 +0700

    remove iife for test/index

--- test/index.js
+++ test/index.js
@@ -1,42 +1,40 @@
 'use strict';
 
-(() => {
-  const unit = require('unit.js');
-  const config = require('../.eslintrc');
+const unit = require('unit.js');
+const config = require('../.eslintrc');
 
-  describe('eslint-config-scuba-squad', () => {
+describe('eslint-config-scuba-squad', () => {
+  it('should be an object hash', () => {
+    unit
+      .object(config)
+      .hasProperty('env')
+      .hasProperty('parserOptions')
+      .hasProperty('rules');
+  }); // end it
+
+  describe('#env', () => {
     it('should be an object hash', () => {
       unit
-        .object(config)
-        .hasProperty('env')
-        .hasProperty('parserOptions')
-        .hasProperty('rules');
+        .object(config.env)
+        .hasProperty('node', true)
+        .hasProperty('es6', true);
     }); // end it
+  }); // end describe #env
 
-    describe('#env', () => {
-      it('should be an object hash', () => {
-        unit
-          .object(config.env)
-          .hasProperty('node', true)
-          .hasProperty('es6', true);
-      }); // end it
-    }); // end describe #env
+  describe('#parserOptions', () => {
+    it('should be an object hash', () => {
+      unit
+        .object(config.parserOptions)
+        .hasProperty('ecmaVersion', 2018)
+        .hasProperty('sourceType', 'script');
+    }); // end it
+  }); // end describe #parserOptions
 
-    describe('#parserOptions', () => {
-      it('should be an object hash', () => {
-        unit
-          .object(config.parserOptions)
-          .hasProperty('ecmaVersion', 2018)
-          .hasProperty('sourceType', 'script');
-      }); // end it
-    }); // end describe #parserOptions
-
-    describe('#rules', () => {
-      it('should be an object hash', () => {
-        unit
-          .object(config.rules)
-          .isNotEmpty();
-      }); // end it
-    }); // end describe #rules
-  }); // end describe eslint-config-scuba-squad
-})(); // end IIFE
+  describe('#rules', () => {
+    it('should be an object hash', () => {
+      unit
+        .object(config.rules)
+        .isNotEmpty();
+    }); // end it
+  }); // end describe #rules
+}); // end describe eslint-config-scuba-squad
